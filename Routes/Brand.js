5a96c9fc83b14b58ed3b5cad8af60d74a621a9e1
Keerthi <keerthana14298@gmail.com>
2024-01-19 12:38:19 +0530

    brand api

---
+++ Routes/Brand.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const dbConnect = require("../config/db");
+const Brands = require("../schema/Brands");
+dbConnect();
+const errMessage = "Something went wrong please try again later";
+
+//! Get All User Data
+router.get("/", async (req, res) => {
+  try {
+    const data = await Brands.find();
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: errMessage }).end();
+  }
+});
+
+//! Get Single User data
+router.get("/:id", async (req, res) => {
+  try {
+    const brandId = req.params.id;
+    const brand = await Brands.findById(brandId);
+
+    if (!brand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    res.json(brand);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: errMessage }).end();
+  }
+});
+
+//! Add  User data
+router.post("/", async (req, res) => {
+  try {
+    await Brands.create(req.body);
+    res.json({ message: "Brand Created Successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: errMessage }).end();
+  }
+});
+
+//! Edit User data
+router.put("/:id", async (req, res) => {
+  try {
+    const brandId = req.params.id;
+    const brandData = req.body;
+
+    const updatedBrand = await Brands.findByIdAndUpdate(brandId, brandData, {
+      new: true,
+    });
+
+    if (!updatedBrand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    res.json({ message: "Brand Updated Successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: errMessage }).end();
+  }
+});
+
+//! Delete User
+router.delete("/:id", async (req, res) => {
+  try {
+    const brandId = req.params.id;
+    const brand = await Brands.findById(brandId);
+
+    if (!brand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    await brand.deleteOne();
+
+    res.json({ message: "Brand deleted successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: errMessage }).end();
+  }
+});
+
+module.exports = router;
